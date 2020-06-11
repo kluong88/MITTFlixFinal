7b30f8cc318c53a0c9ea4360d67274f6ecfea999
Kevin Luong <luongk88@gmail.com>
2020-06-10 19:23:11 -0500

    Clicking on liked icon now adds or removes movie from myList and updates state accordingly to toggle icon

--- src/Movie.js
+++ src/Movie.js
@@ -2,20 +2,20 @@ import React from "react";
 
 export default function Movie(props) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={props.movie.poster_path} />
-      <div class="overlay">
-        <div class="title">{props.movie.title}</div>
-        <div class="rating">{props.movie.vote_average}/10</div>
-        <div class="plot">{props.movie.overview}</div>
+      <div className="overlay">
+        <div className="title">{props.movie.title}</div>
+        <div className="rating">{props.movie.vote_average}/10</div>
+        <div className="plot">{props.movie.overview}</div>
         <div
           data-toggled={props.toggled}
-          class="listToggle"
+          className="listToggle"
           onClick={() => props.toggleMyList(props)}
         >
           <div>
-            <i class="fa fa-fw fa-plus"></i>
-            <i class="fa fa-fw fa-check"></i>
+            <i className="fa fa-fw fa-plus"></i>
+            <i className="fa fa-fw fa-check"></i>
           </div>
         </div>
       </div>
